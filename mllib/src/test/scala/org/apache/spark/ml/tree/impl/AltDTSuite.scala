555d0f6fd8c00eedbf9562a00fbcc43d4b19ee87
Feynman Liang <feynman.liang@gmail.com>
2015-11-14 21:49:37 +0000

    Fixes test featureArity

--- mllib/src/test/scala/org/apache/spark/ml/tree/impl/AltDTSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/tree/impl/AltDTSuite.scala
@@ -233,12 +233,12 @@ class AltDTSuite extends SparkFunSuite with MLlibTestSparkContext  {
 
   test("chooseUnorderedCategoricalSplit: basic case") { }
     val featureIndex = 0
-    val values = Seq(5.0, 1.0, 2.0, 2.0, 3.0)
+    val values = Seq(3.0, 1.0, 0.0, 2.0, 2.0)
     val labels = Seq(0.0, 0.0, 1.0, 1.0, 1.0)
     val impurity = Entropy
     val metadata = new AltDTMetadata(numClasses = 2, maxBins = 4, minInfoGain = 0.0, impurity)
     val (split, stats) = AltDT.chooseUnorderedCategoricalSplit(
-      featureIndex, values, labels, metadata, values.toSet.size)
+      featureIndex, values, labels, metadata, 4)
     split match {
       case Some(s: CategoricalSplit) =>
         assert(s.featureIndex === featureIndex)
@@ -246,7 +246,7 @@ class AltDTSuite extends SparkFunSuite with MLlibTestSparkContext  {
         throw new AssertionError(
           s"Expected CategoricalSplit but got ${split.getClass.getSimpleName}")
     }
-    // TODO: this just exercises the code path without checking anything, add correctness assertions
+    // TODO: this just exercises code path without checking anything, add correctness assertions
   }
 
   //  test("chooseUnorderedCategoricalSplit: return bad split if we should not split") { }
