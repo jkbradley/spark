7669dbfc6acc65a84b82668e47d92474880fafe7
Joseph K. Bradley <joseph@databricks.com>
2016-09-07 18:12:44 -0700

    style fixes

--- mllib/src/main/scala/org/apache/spark/ml/clustering/LDA.scala
+++ mllib/src/main/scala/org/apache/spark/ml/clustering/LDA.scala
@@ -30,26 +30,29 @@ import org.apache.spark.ml.param._
 import org.apache.spark.ml.param.shared.{HasCheckpointInterval, HasFeaturesCol, HasMaxIter, HasSeed}
 import org.apache.spark.ml.util._
 import org.apache.spark.ml.util.DefaultParamsReader.Metadata
-import org.apache.spark.mllib.clustering.{DistributedLDAModel => OldDistributedLDAModel, EMLDAOptimizer => OldEMLDAOptimizer, LDA => OldLDA, LDAModel => OldLDAModel, LDAOptimizer => OldLDAOptimizer, LocalLDAModel => OldLocalLDAModel, OnlineLDAOptimizer => OldOnlineLDAOptimizer}
+import org.apache.spark.mllib.clustering.{DistributedLDAModel => OldDistributedLDAModel,
+  EMLDAOptimizer => OldEMLDAOptimizer, LDA => OldLDA, LDAModel => OldLDAModel,
+  LDAOptimizer => OldLDAOptimizer, LocalLDAModel => OldLocalLDAModel,
+  OnlineLDAOptimizer => OldOnlineLDAOptimizer}
 import org.apache.spark.mllib.impl.PeriodicCheckpointer
-import org.apache.spark.mllib.linalg.{Matrices => OldMatrices, Vector => OldVector, Vectors => OldVectors}
+import org.apache.spark.mllib.linalg.{Vector => OldVector, Vectors => OldVectors}
 import org.apache.spark.mllib.linalg.MatrixImplicits._
 import org.apache.spark.mllib.linalg.VectorImplicits._
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import org.apache.spark.sql.functions.{col, monotonicallyIncreasingId, udf}
+import org.apache.spark.sql.functions.{col, monotonically_increasing_id, udf}
 import org.apache.spark.sql.types.StructType
 
 
 private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasMaxIter
   with HasSeed with HasCheckpointInterval {
 
-   /*
-    * Param for the number of topics (clusters) to infer. Must be > 1. Default: 10.
-    *
-    * @group param
-    */
+  /**
+   * Param for the number of topics (clusters) to infer. Must be > 1. Default: 10.
+   *
+   * @group param
+   */
   @Since("1.6.0")
   final val k = new IntParam(this, "k", "The number of topics (clusters) to infer. " +
     "Must be > 1.", ParamValidators.gt(1))
@@ -58,32 +61,32 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   def getK: Int = $(k)
 
-   /*
-    * Concentration parameter (commonly named "alpha") for the prior placed on documents'
-    * distributions over topics ("theta").
-    *
-    * This is the parameter to a Dirichlet distribution, where larger values mean more smoothing
-    * (more regularization).
-    *
-    * If not set by the user, then docConcentration is set automatically. If set to
-    * singleton vector [alpha], then alpha is replicated to a vector of length k in fitting.
-    * Otherwise, the [[docConcentration]] vector must be length k.
-    * (default = automatic)
-    *
-    * Optimizer-specific parameter settings:
-    * - EM
-    * - Currently only supports symmetric distributions, so all values in the vector should be
-    * the same.
-    * - Values should be > 1.0
-    * - default = uniformly (50 / k) + 1, where 50/k is common in LDA libraries and +1 follows
-    * from Asuncion et al. (2009), who recommend a +1 adjustment for EM.
-    * - Online
-    * - Values should be >= 0
-    * - default = uniformly (1.0 / k), following the implementation from
-    * [[https://github.com/Blei-Lab/onlineldavb]].
-    *
-    * @group param
-    */
+  /**
+   * Concentration parameter (commonly named "alpha") for the prior placed on documents'
+   * distributions over topics ("theta").
+   *
+   * This is the parameter to a Dirichlet distribution, where larger values mean more smoothing
+   * (more regularization).
+   *
+   * If not set by the user, then docConcentration is set automatically. If set to
+   * singleton vector [alpha], then alpha is replicated to a vector of length k in fitting.
+   * Otherwise, the [[docConcentration]] vector must be length k.
+   * (default = automatic)
+   *
+   * Optimizer-specific parameter settings:
+   * - EM
+   * - Currently only supports symmetric distributions, so all values in the vector should be
+   * the same.
+   * - Values should be > 1.0
+   * - default = uniformly (50 / k) + 1, where 50/k is common in LDA libraries and +1 follows
+   * from Asuncion et al. (2009), who recommend a +1 adjustment for EM.
+   * - Online
+   * - Values should be >= 0
+   * - default = uniformly (1.0 / k), following the implementation from
+   * [[https://github.com/Blei-Lab/onlineldavb]].
+   *
+   * @group param
+   */
   @Since("1.6.0")
   final val docConcentration = new DoubleArrayParam(this, "docConcentration",
     "Concentration parameter (commonly named \"alpha\") for the prior placed on documents'" +
@@ -102,30 +105,30 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
     }
   }
 
-   /*
-    * Concentration parameter (commonly named "beta" or "eta") for the prior placed on topics'
-    * distributions over terms.
-    *
-    * This is the parameter to a symmetric Dirichlet distribution.
-    *
-    * Note: The topics' distributions over terms are called "beta" in the original LDA paper
-    * by Blei et al., but are called "phi" in many later papers such as Asuncion et al., 2009.
-    *
-    * If not set by the user, then topicConcentration is set automatically.
-    * (default = automatic)
-    *
-    * Optimizer-specific parameter settings:
-    * - EM
-    * - Value should be > 1.0
-    * - default = 0.1 + 1, where 0.1 gives a small amount of smoothing and +1 follows
-    * Asuncion et al. (2009), who recommend a +1 adjustment for EM.
-    * - Online
-    * - Value should be >= 0
-    * - default = (1.0 / k), following the implementation from
-    * [[https://github.com/Blei-Lab/onlineldavb]].
-    *
-    * @group param
-    */
+  /**
+   * Concentration parameter (commonly named "beta" or "eta") for the prior placed on topics'
+   * distributions over terms.
+   *
+   * This is the parameter to a symmetric Dirichlet distribution.
+   *
+   * Note: The topics' distributions over terms are called "beta" in the original LDA paper
+   * by Blei et al., but are called "phi" in many later papers such as Asuncion et al., 2009.
+   *
+   * If not set by the user, then topicConcentration is set automatically.
+   * (default = automatic)
+   *
+   * Optimizer-specific parameter settings:
+   * - EM
+   * - Value should be > 1.0
+   * - default = 0.1 + 1, where 0.1 gives a small amount of smoothing and +1 follows
+   * Asuncion et al. (2009), who recommend a +1 adjustment for EM.
+   * - Online
+   * - Value should be >= 0
+   * - default = (1.0 / k), following the implementation from
+   * [[https://github.com/Blei-Lab/onlineldavb]].
+   *
+   * @group param
+   */
   @Since("1.6.0")
   final val topicConcentration = new DoubleParam(this, "topicConcentration",
     "Concentration parameter (commonly named \"beta\" or \"eta\") for the prior placed on topic'" +
@@ -148,24 +151,24 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   final val supportedOptimizers: Array[String] = Array("online", "em")
 
-   /*
-    * Optimizer or inference algorithm used to estimate the LDA model.
-    * Currently supported (case-insensitive):
-    * - "online": Online Variational Bayes (default)
-    * - "em": Expectation-Maximization
-    *
-    * For details, see the following papers:
-    * - Online LDA:
-    * Hoffman, Blei and Bach.  "Online Learning for Latent Dirichlet Allocation."
-    * Neural Information Processing Systems, 2010.
-    * [[http://www.cs.columbia.edu/~blei/papers/HoffmanBleiBach2010b.pdf]]
-    * - EM:
-    * Asuncion et al.  "On Smoothing and Inference for Topic Models."
-    * Uncertainty in Artificial Intelligence, 2009.
-    * [[http://arxiv.org/pdf/1205.2662.pdf]]
-    *
-    * @group param
-    */
+  /**
+   * Optimizer or inference algorithm used to estimate the LDA model.
+   * Currently supported (case-insensitive):
+   * - "online": Online Variational Bayes (default)
+   * - "em": Expectation-Maximization
+   *
+   * For details, see the following papers:
+   * - Online LDA:
+   * Hoffman, Blei and Bach.  "Online Learning for Latent Dirichlet Allocation."
+   * Neural Information Processing Systems, 2010.
+   * [[http://www.cs.columbia.edu/~blei/papers/HoffmanBleiBach2010b.pdf]]
+   * - EM:
+   * Asuncion et al.  "On Smoothing and Inference for Topic Models."
+   * Uncertainty in Artificial Intelligence, 2009.
+   * [[http://arxiv.org/pdf/1205.2662.pdf]]
+   *
+   * @group param
+   */
   @Since("1.6.0")
   final val optimizer = new Param[String](this, "optimizer", "Optimizer or inference" +
     " algorithm used to estimate the LDA model. Supported: " + supportedOptimizers.mkString(", "),
@@ -175,16 +178,16 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   def getOptimizer: String = $(optimizer)
 
-   /*
-    * Output column with estimates of the topic mixture distribution for each document (often called
-    * "theta" in the literature).  Returns a vector of zeros for an empty document.
-    *
-    * This uses a variational approximation following Hoffman et al. (2010), where the approximate
-    * distribution is called "gamma."  Technically, this method returns this approximation "gamma"
-    * for each document.
-    *
-    * @group param
-    */
+  /**
+   * Output column with estimates of the topic mixture distribution for each document (often called
+   * "theta" in the literature).  Returns a vector of zeros for an empty document.
+   *
+   * This uses a variational approximation following Hoffman et al. (2010), where the approximate
+   * distribution is called "gamma."  Technically, this method returns this approximation "gamma"
+   * for each document.
+   *
+   * @group param
+   */
   @Since("1.6.0")
   final val topicDistributionCol = new Param[String](this, "topicDistributionCol", "Output column" +
     " with estimates of the topic mixture distribution for each document (often called \"theta\"" +
@@ -196,16 +199,16 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   def getTopicDistributionCol: String = $(topicDistributionCol)
 
-   /*
-    * For Online optimizer only: [[optimizer]] = "online".
-    *
-    * A (positive) learning parameter that downweights early iterations. Larger values make early
-    * iterations count less.
-    * This is called "tau0" in the Online LDA paper (Hoffman et al., 2010)
-    * Default: 1024, following Hoffman et al.
-    *
-    * @group expertParam
-    */
+  /**
+   * For Online optimizer only: [[optimizer]] = "online".
+   *
+   * A (positive) learning parameter that downweights early iterations. Larger values make early
+   * iterations count less.
+   * This is called "tau0" in the Online LDA paper (Hoffman et al., 2010)
+   * Default: 1024, following Hoffman et al.
+   *
+   * @group expertParam
+   */
   @Since("1.6.0")
   final val learningOffset = new DoubleParam(this, "learningOffset", "(For online optimizer)" +
     " A (positive) learning parameter that downweights early iterations. Larger values make early" +
@@ -216,16 +219,16 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   def getLearningOffset: Double = $(learningOffset)
 
-   /*
-    * For Online optimizer only: [[optimizer]] = "online".
-    *
-    * Learning rate, set as an exponential decay rate.
-    * This should be between (0.5, 1.0] to guarantee asymptotic convergence.
-    * This is called "kappa" in the Online LDA paper (Hoffman et al., 2010).
-    * Default: 0.51, based on Hoffman et al.
-    *
-    * @group expertParam
-    */
+  /**
+   * For Online optimizer only: [[optimizer]] = "online".
+   *
+   * Learning rate, set as an exponential decay rate.
+   * This should be between (0.5, 1.0] to guarantee asymptotic convergence.
+   * This is called "kappa" in the Online LDA paper (Hoffman et al., 2010).
+   * Default: 0.51, based on Hoffman et al.
+   *
+   * @group expertParam
+   */
   @Since("1.6.0")
   final val learningDecay = new DoubleParam(this, "learningDecay", "(For online optimizer)" +
     " Learning rate, set as an exponential decay rate. This should be between (0.5, 1.0] to" +
@@ -235,23 +238,23 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   def getLearningDecay: Double = $(learningDecay)
 
-   /*
-    * For Online optimizer only: [[optimizer]] = "online".
-    *
-    * Fraction of the corpus to be sampled and used in each iteration of mini-batch
-    * gradient descent, in range (0, 1].
-    *
-    * Note that this should be adjusted in synch with [[LDA.maxIter]]
-    * so the entire corpus is used.  Specifically, set both so that
-    * maxIterations * miniBatchFraction >= 1.
-    *
-    * Note: This is the same as the `miniBatchFraction` parameter in
-    * [[org.apache.spark.mllib.clustering.OnlineLDAOptimizer]].
-    *
-    * Default: 0.05, i.e., 5% of total documents.
-    *
-    * @group param
-    */
+  /**
+   * For Online optimizer only: [[optimizer]] = "online".
+   *
+   * Fraction of the corpus to be sampled and used in each iteration of mini-batch
+   * gradient descent, in range (0, 1].
+   *
+   * Note that this should be adjusted in synch with [[LDA.maxIter]]
+   * so the entire corpus is used.  Specifically, set both so that
+   * maxIterations * miniBatchFraction >= 1.
+   *
+   * Note: This is the same as the `miniBatchFraction` parameter in
+   * [[org.apache.spark.mllib.clustering.OnlineLDAOptimizer]].
+   *
+   * Default: 0.05, i.e., 5% of total documents.
+   *
+   * @group param
+   */
   @Since("1.6.0")
   final val subsamplingRate = new DoubleParam(this, "subsamplingRate", "(For online optimizer)" +
     " Fraction of the corpus to be sampled and used in each iteration of mini-batch" +
@@ -262,16 +265,16 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   def getSubsamplingRate: Double = $(subsamplingRate)
 
-   /*
-    * For Online optimizer only (currently): [[optimizer]] = "online".
-    *
-    * Indicates whether the docConcentration (Dirichlet parameter for
-    * document-topic distribution) will be optimized during training.
-    * Setting this to true will make the model more expressive and fit the training data better.
-    * Default: false
-    *
-    * @group expertParam
-    */
+  /**
+   * For Online optimizer only (currently): [[optimizer]] = "online".
+   *
+   * Indicates whether the docConcentration (Dirichlet parameter for
+   * document-topic distribution) will be optimized during training.
+   * Setting this to true will make the model more expressive and fit the training data better.
+   * Default: false
+   *
+   * @group expertParam
+   */
   @Since("1.6.0")
   final val optimizeDocConcentration = new BooleanParam(this, "optimizeDocConcentration",
     "(For online optimizer only, currently) Indicates whether the docConcentration" +
@@ -281,21 +284,21 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("1.6.0")
   def getOptimizeDocConcentration: Boolean = $(optimizeDocConcentration)
 
-   /*
-    * For EM optimizer only: [[optimizer]] = "em".
-    *
-    * If using checkpointing, this indicates whether to keep the last
-    * checkpoint. If false, then the checkpoint will be deleted. Deleting the checkpoint can
-    * cause failures if a data partition is lost, so set this bit with care.
-    * Note that checkpoints will be cleaned up via reference counting, regardless.
-    *
-    * See [[DistributedLDAModel.getCheckpointFiles]] for getting remaining checkpoints and
-    * [[DistributedLDAModel.deleteCheckpointFiles]] for removing remaining checkpoints.
-    *
-    * Default: true
-    *
-    * @group expertParam
-    */
+  /**
+   * For EM optimizer only: [[optimizer]] = "em".
+   *
+   * If using checkpointing, this indicates whether to keep the last
+   * checkpoint. If false, then the checkpoint will be deleted. Deleting the checkpoint can
+   * cause failures if a data partition is lost, so set this bit with care.
+   * Note that checkpoints will be cleaned up via reference counting, regardless.
+   *
+   * See [[DistributedLDAModel.getCheckpointFiles]] for getting remaining checkpoints and
+   * [[DistributedLDAModel.deleteCheckpointFiles]] for removing remaining checkpoints.
+   *
+   * Default: true
+   *
+   * @group expertParam
+   */
   @Since("2.0.0")
   final val keepLastCheckpoint = new BooleanParam(this, "keepLastCheckpoint",
     "(For EM optimizer) If using checkpointing, this indicates whether to keep the last" +
@@ -306,12 +309,12 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
   @Since("2.0.0")
   def getKeepLastCheckpoint: Boolean = $(keepLastCheckpoint)
 
-   /*
-    * Validates and transforms the input schema.
-    *
-    * @param schema input schema
-    * @return output schema
-    */
+  /**
+   * Validates and transforms the input schema.
+   *
+   * @param schema input schema
+   * @return output schema
+   */
   protected def validateAndTransformSchema(schema: StructType): StructType = {
     if (isSet(docConcentration)) {
       if (getDocConcentration.length != 1) {
@@ -356,8 +359,7 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
         .setKeepLastCheckpoint($(keepLastCheckpoint))
   }
 
-  def extractTopicDistributionCol(metadata: Metadata, model: Params): Unit =
-  {
+  def extractTopicDistributionCol(metadata: Metadata, model: Params): Unit = {
     implicit val format = DefaultFormats
     metadata.params match {
       case JObject(pairs) =>
@@ -373,7 +375,6 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
           s"Cannot recognize JSON metadata: ${metadata.metadataJson}.")
     }
   }
-
 }
 
 
@@ -934,7 +935,7 @@ object LDA extends DefaultParamsReadable[LDA] {
        dataset: Dataset[_],
        featuresCol: String): RDD[(Long, OldVector)] = {
     dataset
-      .withColumn("docId", monotonicallyIncreasingId())
+      .withColumn("docId", monotonically_increasing_id())
       .select("docId", featuresCol)
       .rdd
       .map { case Row(docId: Long, features: Vector) =>
